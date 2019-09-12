d3aad5d4a63324c801c68bc1537d9f3832321439
cornel <ghita_cornel@yahoo.com>
2019-09-12 22:35:31 +0300

    xxx

--- testJUnit/src/main/java/junit/a/simple/IService.java
+++ testJUnit/src/main/java/junit/a/simple/IService.java
@@ -2,19 +2,15 @@ package junit.a.simple;
 
 /**
  * This is a contract<br>
- * Our application will implement it
- * 
- * @author CornelGhita
- * 
+ * Our application must implement it
  */
 public interface IService {
 
-	/**
-	 * 
-	 * @param a
-	 * @param b
-	 * @return a+b
-	 */
-	int sum(int a, int b);
+    /**
+     * @param a
+     * @param b
+     * @return a+b
+     */
+    int sum(int a, int b);
 
 }
