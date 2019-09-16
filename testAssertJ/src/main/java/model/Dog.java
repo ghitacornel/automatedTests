1bb1370b07a9820b96cb54c5d11715ea4dec5a8c
ghitacornel <ghita.cornel@gmail.com>
2019-09-16 10:58:41 +0300

    xxx

---
+++ testAssertJ/src/main/java/model/Dog.java
@@ -0,0 +1,15 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Dog {
+
+    private String name;
+    private double weight;
+
+}
---
+++ testAssertJ/src/test/java/asserts/TestAsserts.java
@@ -0,0 +1,112 @@
+package asserts;
+
+import model.Dog;
+import org.assertj.core.api.Assertions;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.withPrecision;
+import static org.assertj.core.data.MapEntry.entry;
+
+public class TestAsserts {
+
+    @Rule
+    public TemporaryFolder temporaryFolderRule = new TemporaryFolder();
+
+    @Test
+    public void testAsserts() throws Exception {
+
+        {
+
+            Assertions.assertThat(true).isTrue();
+            Assertions.assertThat(false).isFalse();
+
+        }
+
+        {
+
+            Assertions.assertThat(1).isEqualTo(1);
+            Assertions.assertThat(2).isNotEqualTo(3);
+            Assertions.assertThat(4).isPositive();
+            Assertions.assertThat(-4).isNotPositive();
+            Assertions.assertThat(-5).isNegative();
+            Assertions.assertThat(6).isNotNegative();
+            Assertions.assertThat(0).isZero();
+            Assertions.assertThat(7).isNotZero();
+            assertThat(5.1).isEqualTo(5, withPrecision(1d));
+
+        }
+
+        {
+
+            Object o1 = null;
+            Assertions.assertThat(o1).isNull();
+
+            Object o2 = new Object();
+            Assertions.assertThat(o2).isNotNull();
+
+            Object o3 = new Object();
+            Object o4 = o3;
+            Assertions.assertThat(o3).isSameAs(o4);
+
+            Object o5 = new Object();
+            Object o6 = new Object();
+            Assertions.assertThat(o5).isNotSameAs(o6);
+
+            Dog fido = new Dog("Fido", 5.25);
+            Dog fidosClone = new Dog("Fido", 5.25);
+            assertThat(fido).isEqualToComparingFieldByFieldRecursively(fidosClone);
+
+        }
+
+        {
+            List<String> list = Arrays.asList("1", "2", "3");
+            assertThat(list).contains("1");
+            assertThat(list).isNotEmpty();
+            assertThat(list).startsWith("1");
+            assertThat(list)
+                    .isNotEmpty()
+                    .contains("1")
+                    .doesNotContainNull()
+                    .containsSequence("2", "3");
+
+            Map<Integer, String> map = new HashMap<>();
+            map.put(2, "a");
+            assertThat(map)
+                    .isNotEmpty()
+                    .containsKey(2)
+                    .doesNotContainKeys(10)
+                    .contains(entry(2, "a"));
+
+        }
+
+        {
+            char someCharacter = 'x';
+            assertThat(someCharacter)
+                    .isNotEqualTo('a')
+                    .inUnicode()
+                    .isGreaterThanOrEqualTo('b')
+                    .isLowerCase();
+        }
+
+        {
+            assertThat(Runnable.class).isInterface();
+            assertThat(Exception.class).isAssignableFrom(NoSuchElementException.class);
+        }
+
+        {
+            File temporaryFile = temporaryFolderRule.newFile("temporaryFile.txt");
+            assertThat(temporaryFile)
+                    .exists()
+                    .isFile()
+                    .canRead()
+                    .canWrite();
+        }
+
+    }
+}
