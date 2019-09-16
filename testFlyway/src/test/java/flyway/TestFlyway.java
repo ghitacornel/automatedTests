a061c32a7a636499109c6172f741f3205d77ff0d
ghitacornel <ghita.cornel@gmail.com>
2019-09-16 14:08:49 +0300

    xxx

--- testFlyway/src/test/java/flyway/TestFlyway.java
+++ testFlyway/src/test/java/flyway/TestFlyway.java
@@ -1,24 +1,36 @@
 package flyway;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TestFlyway extends TestsSetUp {
 
     @Test
     public void testIt() throws Exception {
 
-        flyway.validate();
+        List<Integer> ints = new ArrayList<>();
+        List<String> strings = new ArrayList<>();
 
-        Connection connection = flyway.getConfiguration().getDataSource().getConnection();
-        ResultSet resultSet = connection.createStatement().executeQuery("select * from testtable");
-        while (resultSet.next()) {
-            int anInt = resultSet.getInt(1);
-            String aString = resultSet.getString(2);
-            System.out.println(anInt + " " + aString);
+        {
+            Connection connection = flyway.getConfiguration().getDataSource().getConnection();
+            ResultSet resultSet = connection.createStatement().executeQuery("select * from testtable");
+            while (resultSet.next()) {
+                int anInt = resultSet.getInt(1);
+                ints.add(anInt);
+                String aString = resultSet.getString(2);
+                strings.add(aString);
+            }
+            connection.close();
         }
-        connection.close();
+
+        Assert.assertEquals(ints, Arrays.asList(1, 2, 3));
+        Assert.assertEquals(strings, Arrays.asList("d1", "d2", null));
+
     }
 }
