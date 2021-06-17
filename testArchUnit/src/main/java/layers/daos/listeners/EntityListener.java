334356476b1555daa0ed9ebe80cf4e37de95b9fc
cornel <ghita.cornel@gmail.com>
2021-06-17 22:47:40 +0300

    better

--- testArchUnit/src/main/java/layers/daos/listeners/EntityListener.java
+++ testArchUnit/src/main/java/layers/daos/listeners/EntityListener.java
@@ -1,11 +1,17 @@
 package layers.daos.listeners;
 
+import layers.daos.entities.EntityTemplate;
+
 public class EntityListener {
 
     public EntityListener() {
     }
 
     public EntityListener(int x) {
+    }
+
+    public void prePersist(EntityTemplate entity) {
+
     }
 
 }
--- testArchUnit/src/test/java/tests/layers/daos/listeners/TestListeners.java
+++ testArchUnit/src/test/java/tests/layers/daos/listeners/TestListeners.java
@@ -2,6 +2,7 @@ package tests.layers.daos.listeners;
 
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
+import layers.daos.entities.EntityTemplate;
 import org.junit.Test;
 import specials.MethodHasExactlyOneParameter;
 import specials.ClassHasOnePublicNoArgumentsConstructor;
@@ -26,7 +27,7 @@ public class TestListeners {
     public void testMethods() {
         methods()
                 .should().haveRawReturnType(void.class)
-                .andShould(new MethodHasExactlyOneParameter())
+                .andShould(new MethodHasExactlyOneParameter(EntityTemplate.class))
                 .check(classes);
     }
 
