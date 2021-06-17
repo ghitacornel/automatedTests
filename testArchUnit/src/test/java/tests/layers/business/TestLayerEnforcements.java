8a6aafbc9401f7494759330a496882fe637a42b5
cornel <ghita.cornel@gmail.com>
2021-06-18 00:25:14 +0300

    better

--- testArchUnit/src/test/java/tests/layers/business/TestLayerEnforcements.java
+++ testArchUnit/src/test/java/tests/layers/business/TestLayerEnforcements.java
@@ -6,13 +6,14 @@ import org.junit.Test;
 import thirdpartydependencies.business.Component;
 import thirdpartydependencies.business.services.Service;
 import thirdpartydependencies.business.Utility;
+import thirdpartydependencies.configurations.Configuration;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 public class TestLayerEnforcements {
 
-    JavaClasses classes = new ClassFileImporter().importPackages("daos");
+    JavaClasses classes = new ClassFileImporter().importPackages("business");
 
     @Test
     public void testNamingConventionsByPackageName() {
@@ -36,12 +37,19 @@ public class TestLayerEnforcements {
     public void testClassSameLayerAccess() {
 
         noClasses().that().resideInAPackage("..model..")
-                .should().dependOnClassesThat().resideInAPackage("..services..").check(classes);
-        noClasses().that().resideInAPackage("..utility..")
-                .should().dependOnClassesThat().resideInAPackage("..services..").check(classes);
+                .should().dependOnClassesThat().areAnnotatedWith(Service.class)
+                .check(classes);
+        noClasses().that().resideInAPackage("..model..")
+                .should().dependOnClassesThat().areAnnotatedWith(Configuration.class)
+                .check(classes);
 
+        noClasses().that().resideInAPackage("..utility..")
+                .should().dependOnClassesThat().resideInAPackage("..services..")
+                .check(classes);
         classes().that().resideInAPackage("..utility..")
-                .should().onlyBeAccessed().byClassesThat().resideInAPackage("..services..");
+                .should().onlyBeAccessed().byClassesThat().resideInAPackage("..services..")
+                .check(classes);
+
         classes().that().areAnnotatedWith(Component.class)
                 .should().onlyBeAccessed().byClassesThat().areAnnotatedWith(Component.class)
                 .orShould().onlyBeAccessed().byClassesThat().areAnnotatedWith(Utility.class)
