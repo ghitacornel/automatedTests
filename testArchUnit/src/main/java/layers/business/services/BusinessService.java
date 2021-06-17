bbf93033b1285e99535f024f71d65a5ad103b3e0
cornel <ghita.cornel@gmail.com>
2021-06-17 23:59:09 +0300

    better

--- testArchUnit/src/main/java/layers/business/services/BusinessService.java
+++ testArchUnit/src/main/java/layers/business/services/BusinessService.java
@@ -1,7 +1,13 @@
 package layers.business.services;
 
 import thirdpartydependencies.business.services.Service;
+import thirdpartydependencies.business.services.Transactional;
 
 @Service
 public class BusinessService {
+
+    @Transactional
+    public void businessService() {
+    }
+
 }
--- testArchUnit/src/test/java/tests/layers/business/services/TestServices.java
+++ testArchUnit/src/test/java/tests/layers/business/services/TestServices.java
@@ -2,28 +2,32 @@ package tests.layers.business.services;
 
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 import org.junit.Test;
-import thirdpartydependencies.business.Component;
 import thirdpartydependencies.business.services.Service;
-import thirdpartydependencies.business.Utility;
+import thirdpartydependencies.business.services.Transactional;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods;
 
 public class TestServices {
 
-    JavaClasses classes = new ClassFileImporter().importPackages("layers.services");
+    JavaClasses classes = new ClassFileImporter().importPackages("layers.business");
 
     @Test
     public void testClasses() {
         classes().that()
-                .resideOutsideOfPackage("..model..")
-                .and()
-                .areNotAnnotatedWith(Component.class)
-                .and()
-                .areNotAnnotatedWith(Utility.class)
+                .areAnnotatedWith(Service.class)
                 .should().bePublic()
                 .andShould().haveSimpleNameEndingWith("Service")
-                .andShould().beAnnotatedWith(Service.class)
+                .check(classes);
+    }
+
+    @Test
+    public void testMethods() {
+        methods().that()
+                .areAnnotatedWith(Transactional.class)
+                .should().bePublic()
                 .check(classes);
     }
 
