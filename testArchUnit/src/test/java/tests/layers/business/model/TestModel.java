b6139d0863170add93ad9f617c5d95b8a6d04387
cornel <ghita.cornel@gmail.com>
2021-06-18 00:29:05 +0300

    better

--- testArchUnit/src/test/java/tests/layers/business/model/TestModel.java
+++ testArchUnit/src/test/java/tests/layers/business/model/TestModel.java
@@ -3,10 +3,12 @@ package tests.layers.business.model;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import org.junit.Test;
+import thirdpartydependencies.business.services.Transactional;
 
 import java.io.Serializable;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noMethods;
 
 public class TestModel {
 
@@ -17,6 +19,12 @@ public class TestModel {
         classes()
                 .should().bePublic()
                 .andShould().implement(Serializable.class)
+                .check(classes);
+    }
+
+    @Test
+    public void testMethods() {
+        noMethods().should().beAnnotatedWith(Transactional.class)
                 .check(classes);
     }
 
