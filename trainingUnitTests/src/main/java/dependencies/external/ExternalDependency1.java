e03ff4e1bf94b04fe30216f44a03bd94e32dc75b
cornel <ghita.cornel@gmail.com>
2021-07-04 11:51:12 +0300

    better

--- trainingUnitTests/src/main/java/dependencies/external/ExternalDependency1.java
+++ trainingUnitTests/src/main/java/dependencies/external/ExternalDependency1.java
@@ -1,7 +1,7 @@
 package dependencies.external;
 
 // hint => @Service
-public class ExternalDependency1 {
+class ExternalDependency1 {
 
     public void validate(int x, int y) {
         if (x < 0 || y < 0) throw new RuntimeException("Negative parameter");
--- trainingUnitTests/src/main/java/dependencies/external/ExternalDependency2.java
+++ trainingUnitTests/src/main/java/dependencies/external/ExternalDependency2.java
@@ -1,7 +1,7 @@
 package dependencies.external;
 
 // hint => @Service
-public class ExternalDependency2 {
+class ExternalDependency2 {
 
     public int calculateSpecific(int x, int y) {
         return (x + y) / 2;
