fc6aa6f6d53e35ef42198a56e507bf74d833c424
cornel <ghita.cornel@gmail.com>
2022-03-22 18:56:08 +0200

    better

--- testCucumber/src/test/java/cucumber/StepsDefinition.java
+++ testCucumber/src/test/java/cucumber/StepsDefinition.java
@@ -11,17 +11,17 @@ public class StepsDefinition {
     Belly belly = new Belly();
 
     @Given("^I have (\\d+) cukes in my belly$")
-    public void i_have_cukes_in_my_belly(int arg1) throws Exception {
+    public void i_have_cukes_in_my_belly(int arg1) {
         belly.eat(arg1);
     }
 
     @When("^I wait (\\d+) hour$")
-    public void i_wait_hour(int arg1) throws Exception {
+    public void i_wait_hour(int arg1) {
         belly.wait(arg1);
     }
 
     @Then("^my belly should growl$")
-    public void my_belly_should_growl() throws Exception {
+    public void my_belly_should_growl() {
         belly.growl();
         Assert.assertEquals(42, belly.getCukes());
     }
