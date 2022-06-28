ac5b8fc53be813b06e563a676637e90448c85985
cornel <ghita.cornel@gmail.com>
2022-06-28 15:53:36 +0300

    better

--- powermock/src/test/java/service/BusinessServiceTest.java
+++ powermock/src/test/java/service/BusinessServiceTest.java
@@ -5,18 +5,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
 
-import static org.powermock.api.mockito.PowerMockito.when;
+import static org.powermock.api.mockito.PowerMockito.*;
 
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(BusinessService.class)
 public class BusinessServiceTest {
 
-    BusinessService businessService = new BusinessService();
-
     @Test
-    public void testBusinessMethod() throws Exception {
+    public void testBusinessMethodMockingPrivateMethod() throws Exception {
+        BusinessService businessService = spy(new BusinessService());
 
         when(businessService, "privateMethod", "in").thenReturn("mockedResult");
 
@@ -24,6 +24,14 @@ public class BusinessServiceTest {
 
         String result = businessService.businessMethod(input);
 
-        Assert.assertEquals(input + input + input + input, result);
+        Assert.assertEquals("inmockedResult", result);
+        verifyPrivate(businessService).invoke("privateMethod", "in");
+    }
+
+    @Test
+    public void testPrivateMethod() throws Exception {
+        BusinessService businessService = new BusinessService();
+        Object result = Whitebox.invokeMethod(businessService, "privateMethod", "in");
+        Assert.assertEquals("ininin", result);
     }
 }
