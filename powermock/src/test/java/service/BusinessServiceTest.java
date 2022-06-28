d2b80a4ddd6154edf0c371db79b0a3c800904dde
cornel <ghita.cornel@gmail.com>
2022-06-28 15:36:49 +0300

    better

--- powermock/src/test/java/service/BusinessServiceTest.java
+++ powermock/src/test/java/service/BusinessServiceTest.java
@@ -1,18 +1,29 @@
 package service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import static org.powermock.api.mockito.PowerMockito.when;
+
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(BusinessService.class)
 public class BusinessServiceTest {
 
     BusinessService businessService = new BusinessService();
 
     @Test
-    public void testBusinessMethod() {
+    public void testBusinessMethod() throws Exception {
+
+        when(businessService, "privateMethod", "in").thenReturn("mockedResult");
+
         String input = "in";
 
         String result = businessService.businessMethod(input);
 
-        Assertions.assertEquals(input + input + input + input, result);
+        Assert.assertEquals(input + input + input + input, result);
     }
 }
