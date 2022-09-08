a483d638634c6fd15680f4919ea59d3b0cd228ce
cornel <ghita.cornel@gmail.com>
2022-09-08 16:25:03 +0300

    better

--- testArchUnit/src/test/java/architecture/controller/ControllerAccessEnforcementConventionTest.java
+++ testArchUnit/src/test/java/architecture/controller/ControllerAccessEnforcementConventionTest.java
@@ -14,10 +14,14 @@ public class ControllerAccessEnforcementConventionTest {
     public void deny() {
         noClasses().that().resideInAPackage(Packages.CONTROLLER)
                 .should().dependOnClassesThat().resideInAPackage(Packages.CONTROLLER)
+                .orShould().dependOnClassesThat().resideInAPackage(Packages.CONTROLLER_ADVICE)
                 .orShould().dependOnClassesThat().resideInAPackage(Packages.MAPPER)
                 .orShould().dependOnClassesThat().resideInAPackage(Packages.EXCEPTION)
                 .orShould().dependOnClassesThat().resideInAPackage(Packages.PERSISTENCE)
-                // TODO
+                .orShould().dependOnClassesThat().resideInAPackage(Packages.PERSISTENCE_CONVERTER)
+                .orShould().dependOnClassesThat().resideInAPackage(Packages.PERSISTENCE_LISTENER)
+                .orShould().dependOnClassesThat().resideInAPackage(Packages.PERSISTENCE_ENTITY)
+                .orShould().dependOnClassesThat().resideInAPackage(Packages.PERSISTENCE_REPOSITORY)
                 .check(classes);
     }
 
--- testArchUnit/src/test/java/architecture/controller/handler/HandlerAccessEnforcementConventionTest.java
+++ testArchUnit/src/test/java/architecture/controller/handler/HandlerAccessEnforcementConventionTest.java
@@ -8,14 +8,16 @@ import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 public class HandlerAccessEnforcementConventionTest {
 
-    private final JavaClasses classes = Packages.allClasses;
+    private final JavaClasses classes = Packages.allClassesWithoutTests;
 
     @Test
     public void deny() {
         noClasses().that().resideInAPackage(Packages.CONTROLLER_ADVICE)
                 .should().dependOnClassesThat().resideInAPackage(Packages.CONTROLLER)
                 .orShould().dependOnClassesThat().resideInAPackage(Packages.PERSISTENCE)
-                // TODO
+                .orShould().dependOnClassesThat().resideInAPackage(Packages.PERSISTENCE_CONVERTER)
+                .orShould().dependOnClassesThat().resideInAPackage(Packages.PERSISTENCE_LISTENER)
+                .orShould().dependOnClassesThat().resideInAPackage(Packages.PERSISTENCE_REPOSITORY)
                 .check(classes);
     }
 
