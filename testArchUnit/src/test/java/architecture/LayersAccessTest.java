0a140616f99cb1c29816b9c62b65a688bbac2f3c
cornel <ghita.cornel@gmail.com>
2022-09-08 17:50:06 +0300

    better

---
+++ testArchUnit/src/test/java/architecture/LayersAccessTest.java
@@ -0,0 +1,43 @@
+package architecture;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import org.junit.Test;
+
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
+public class LayersAccessTest {
+
+    private final JavaClasses classes = Packages.allClassesWithoutTests;
+
+    @Test
+    public void testLayersDependencies() {
+        layeredArchitecture()
+                .layer("Controller").definedBy(Packages.CONTROLLER, Packages.CONTROLLER_ADVICE)
+                .layer("Service").definedBy(Packages.SERVICE + "..")
+                .layer("Repository").definedBy(Packages.PERSISTENCE + "..", Packages.PERSISTENCE_CONVERTER, Packages.PERSISTENCE_LISTENER, Packages.PERSISTENCE_ENTITY, Packages.PERSISTENCE_REPOSITORY)
+                .whereLayer("Controller").mayNotBeAccessedByAnyLayer()
+                .whereLayer("Service").mayOnlyBeAccessedByLayers("Controller", "Service")
+                .whereLayer("Repository").mayOnlyBeAccessedByLayers("Service")
+                .check(classes);
+    }
+
+    @Test
+    public void testAllLayersDependencies() {
+        layeredArchitecture()
+                .layer("Config").definedBy(Packages.CONFIG)
+                .layer("Exception").definedBy(Packages.EXCEPTION)
+                .layer("Mapper").definedBy(Packages.MAPPER)
+                .layer("Model").definedBy(Packages.MODEL)
+                .layer("ControllerAdvice").definedBy(Packages.CONTROLLER_ADVICE)
+                .layer("Controller").definedBy(Packages.CONTROLLER)
+                .layer("Service").definedBy(Packages.SERVICE + "..")
+                .layer("Repository").definedBy(Packages.PERSISTENCE + "..", Packages.PERSISTENCE_CONVERTER, Packages.PERSISTENCE_LISTENER, Packages.PERSISTENCE_ENTITY, Packages.PERSISTENCE_REPOSITORY)
+                .whereLayer("ControllerAdvice").mayNotBeAccessedByAnyLayer()
+                .whereLayer("Mapper").mayOnlyBeAccessedByLayers("Service")
+                .whereLayer("Controller").mayNotBeAccessedByAnyLayer()
+                .whereLayer("Service").mayOnlyBeAccessedByLayers("Controller", "Service")
+                .whereLayer("Repository").mayOnlyBeAccessedByLayers("Service")
+                .check(classes);
+    }
+
+}
