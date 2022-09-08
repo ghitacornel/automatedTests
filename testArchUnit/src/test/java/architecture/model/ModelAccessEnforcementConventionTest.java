804a4fe77f9c8364fc9ad2eca8db8545659c7e0b
cornel <ghita.cornel@gmail.com>
2022-09-08 17:21:37 +0300

    better

---
+++ testArchUnit/src/test/java/architecture/model/ModelAccessEnforcementConventionTest.java
@@ -0,0 +1,29 @@
+package architecture.model;
+
+import architecture.Packages;
+import com.tngtech.archunit.core.domain.JavaClasses;
+import org.junit.Test;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+public class ModelAccessEnforcementConventionTest {
+
+    private final JavaClasses classes = Packages.allClassesWithoutTests;
+
+    @Test
+    public void deny() {
+        noClasses().that().resideInAPackage(Packages.MODEL)
+                .should().dependOnClassesThat().resideInAPackage(Packages.CONFIG)
+                .orShould().dependOnClassesThat().resideInAPackage(Packages.CONTROLLER)
+                .orShould().dependOnClassesThat().resideInAPackage(Packages.CONTROLLER_ADVICE)
+                .orShould().dependOnClassesThat().resideInAPackage(Packages.MAPPER)
+                .orShould().dependOnClassesThat().resideInAPackage(Packages.SERVICE)
+                .orShould().dependOnClassesThat().resideInAPackage(Packages.PERSISTENCE)
+                .orShould().dependOnClassesThat().resideInAPackage(Packages.PERSISTENCE_CONVERTER)
+                .orShould().dependOnClassesThat().resideInAPackage(Packages.PERSISTENCE_LISTENER)
+                .orShould().dependOnClassesThat().resideInAPackage(Packages.PERSISTENCE_ENTITY)
+                .orShould().dependOnClassesThat().resideInAPackage(Packages.PERSISTENCE_REPOSITORY)
+                .check(classes);
+    }
+
+}
