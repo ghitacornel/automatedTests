b254beed510b7201fb5b06fd118a74d1ba9e126d
ghita <ghita.cornel@gmail.com>
2023-06-06 22:06:25 +0300

    better

--- trainingUnitTests/src/main/java/dependencies/privates/PrivateDependenciesService.java
+++ trainingUnitTests/src/main/java/dependencies/privates/PrivateDependenciesService.java
@@ -11,7 +11,7 @@ public class PrivateDependenciesService {
     private void validate(int x, int y) {
         if (x < 0 || y < 0) throw new RuntimeException("Negative parameter");
     }
-    
+
     private int calculateSpecific(int x, int y) {
         return (x + y) / 2;
     }
@@ -19,5 +19,8 @@ public class PrivateDependenciesService {
     // OBSERVE private methods are tested through the exposed business method
     // OBSERVE private methods add testing complexity to the exposed business method
     // in some cases PARAMETERS MATRIX based I/O can be used => Parameterised tests
+
+    // in order to allow individual testing for such private methods
+    // we can use the approach of having these private methods made default and moved to default helper/companion/utility classes
 
 }
