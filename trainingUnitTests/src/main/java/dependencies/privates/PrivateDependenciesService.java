5ca1d3d03cd9753f68d02f6d097be3038ca77653
ghita <ghita.cornel@gmail.com>
2023-06-06 22:09:44 +0300

    better

--- trainingUnitTests/src/main/java/dependencies/privates/PrivateDependenciesService.java
+++ trainingUnitTests/src/main/java/dependencies/privates/PrivateDependenciesService.java
@@ -20,11 +20,13 @@ public class PrivateDependenciesService {
     // hence we are not allowed to unit test them
     // even if we could use reflection for invoking them
 
-    // OBSERVE private methods are tested through the exposed business method
-    // OBSERVE private methods add testing complexity to the exposed business method
+    // OBSERVE private methods are tested through the exposed business methods that use them
+    // OBSERVE private methods become part of the exposed business methods that use them
+    // OBSERVE private methods add testing complexity to the exposed business methods that use them
     // in some cases PARAMETERS MATRIX based I/O can be used => Parameterised tests
 
     // in order to make possible unit testing of such private methods
-    // we can use the approach of having these private methods made default and moved to default helper/companion/utility classes
+    // we can use the approach of having these private methods made default
+    // and moved to default helper/companion/utility classes
 
 }
