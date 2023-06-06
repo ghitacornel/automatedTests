8e2aed6aa3b923aa16f25e9568e17d6c03088123
ghita <ghita.cornel@gmail.com>
2023-06-06 22:02:36 +0300

    better

---
+++ trainingUnitTests/src/main/java/exercise1/SimpleService.java
@@ -0,0 +1,31 @@
+package exercise1;
+
+import java.util.stream.IntStream;
+
+public class SimpleService {
+
+    // simple, linear
+    public int simpleBusinessMethod(int a, int b) {
+        return a + b;
+    }
+
+    // simple but with multiple execution paths
+    // need to identify each execution path and test them individually
+    public int businessMethodWithMultipleExecutionPaths(int x) {
+        return x < 0 ? -x : x;
+    }
+
+    // multiple execution paths
+    // some execution paths end with exception raised
+    // need to identify each execution path and test them individually
+    // need to verify the raised exception type and message
+    public int businessMethodWithExceptions(int x) {
+        if (x < 0) {
+            throw new RuntimeException("negative value");
+        }
+        if (x == 0) {
+            return 1;
+        }
+        return IntStream.range(1, x + 1).reduce(1, (a, b) -> a * b);
+    }
+}
---
+++ trainingUnitTests/src/test/java/exercise1/SimpleServiceTest.java
@@ -0,0 +1,173 @@
+package exercise1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SimpleServiceTest {
+
+    @Test
+    public void simpleBusinessMethod() {
+
+        // step 1 = GIVEN
+        // create test context
+        SimpleService simpleService = new SimpleService();
+
+        // step 1 - create input data
+        int a = 1;
+        int b = 2;
+
+        // step 1 - create expected output data
+        int expectedResult = 3;
+
+        // step 2 = WHEN
+        // invoke
+        // collect return if available
+        int actualResult = simpleService.simpleBusinessMethod(a, b);
+
+        // step 3 = THEN
+        // validate
+
+        // step 3 - validate expected output vs actual output
+        Assert.assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void businessMethodWithMultipleExecutionPaths_PATH_1() {
+
+        // step 1 = GIVEN
+        // create test context
+        SimpleService simpleService = new SimpleService();
+
+        // step 1 - create input data
+        int x = 1;
+
+        // step 1 - create expected output data
+        int expectedResult = 1;
+
+        // step 2 = WHEN
+        // invoke
+        // collect return if available
+        int actualResult = simpleService.businessMethodWithMultipleExecutionPaths(x);
+
+        // step 3 = THEN
+        // validate
+
+        // step 3 - validate expected output vs actual output
+        Assert.assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void businessMethodWithMultipleExecutionPaths_PATH_2() {
+
+        // step 1 = GIVEN
+        // create test context
+        SimpleService simpleService = new SimpleService();
+
+        // step 1 - create input data
+        int x = -1;
+
+        // step 1 - create expected output data
+        int expectedResult = 1;
+
+        // step 2 = WHEN
+        // invoke
+        // collect return if available
+        int actualResult = simpleService.businessMethodWithMultipleExecutionPaths(x);
+
+        // step 3 = THEN
+        // validate
+
+        // step 3 - validate expected output vs actual output
+        Assert.assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void businessMethodWithExceptions_PATH_1() {
+
+        // step 1 = GIVEN
+        // create test context
+        SimpleService simpleService = new SimpleService();
+
+        // step 1 - create input data
+        int x = 0;
+
+        // step 1 - create expected output data
+        int expectedResult = 1;
+
+        // step 2 = WHEN
+        // invoke
+        // collect return if available
+        int actualResult = simpleService.businessMethodWithExceptions(x);
+
+        // step 3 = THEN
+        // validate
+
+        // step 3 - validate expected output vs actual output
+        Assert.assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void businessMethodWithExceptions_PATH_2() {
+
+        // step 1 = GIVEN
+        // create test context
+        SimpleService simpleService = new SimpleService();
+
+        // step 1 - create input data
+        int x = 3;
+
+        // step 1 - create expected output data
+        int expectedResult = 6;
+
+        // step 2 = WHEN
+        // invoke
+        // collect return if available
+        int actualResult = simpleService.businessMethodWithExceptions(x);
+
+        // step 3 = THEN
+        // validate
+
+        // step 3 - validate expected output vs actual output
+        Assert.assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void businessMethodWithExceptions_PATH_Exception() {
+
+        // step 1 = GIVEN
+        // create test context
+        SimpleService simpleService = new SimpleService();
+
+        // step 1 - create input data
+        int x = -1;
+
+        // step 1 - create expected output data
+        String message = "negative value";
+
+        // step 2 = WHEN
+        // invoke
+        // collect return if available
+        // this time the return is an exception
+        try {
+            simpleService.businessMethodWithExceptions(x);
+            Assert.fail("expected exception");
+        } catch (Exception e) {
+
+            // step 3 = THEN
+            // validate
+
+            // step 3 - validate expected output vs actual output
+            // for exceptions need to verify exception type and exception message/content
+            Assert.assertSame(RuntimeException.class, e.getClass());
+            Assert.assertEquals(message, e.getMessage());
+
+        }
+
+    }
+
+}
