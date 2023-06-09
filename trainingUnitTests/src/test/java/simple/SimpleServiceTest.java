e6e773dad3b93f5112e045ca52e4d53aec07994b
ghita <ghita.cornel@gmail.com>
2023-06-09 16:08:10 +0300

    better

--- trainingUnitTests/src/test/java/simple/SimpleServiceTest.java
+++ trainingUnitTests/src/test/java/simple/SimpleServiceTest.java
@@ -7,6 +7,7 @@ public class SimpleServiceTest {
 
     // https://en.wikipedia.org/wiki/Unit_testing
     // we test 1 method in isolation which is achieved using mocks when needed
+    // we test 1 method in isolation which means test context is not to be shared
     // OBSERVE : I don't need to think/understand what the code is supposed to do
     // OBSERVE : I assume the code does what is supposed to do
     // OBSERVE : I write the unit test to ensure that the actual code does what is meant to do now
