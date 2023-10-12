8909712f6e4df875875fe208187e5e0e1ab86aab
ghita <ghita.cornel@gmail.com>
2023-10-12 13:47:19 +0300

    better

---
+++ trainingUnitTests/src/main/java/dependencies/privates/ServiceWithPrivateDependencies.java
@@ -0,0 +1,40 @@
+package dependencies.privates;
+
+public class ServiceWithPrivateDependencies {
+
+    // complex business method that delegates to private methods
+    public int complexBusiness(int x, int y) {
+        validate(x, y);
+        if (x < 10 || y < 10) return calculateSpecific(x, y);
+        return x + y;
+    }
+
+    private void validate(int x, int y) {
+        if (x < 0 || y < 0) throw new RuntimeException("Negative parameter");
+    }
+
+    private int calculateSpecific(int x, int y) {
+        return (x + y) / 2;
+    }
+
+    // PRIVATE methods "DO NOT EXIST"
+    // hence we are not allowed to unit test them
+    // even if we could use reflection for invoking them
+    // testing them means acknowledging their existence and leaving no room for further transparent changes
+
+    // OBSERVE private methods are tested through the exposed business methods that use them
+    // OBSERVE private methods become part of the exposed business methods that use them
+    // OBSERVE private methods add testing complexity to the exposed business methods that use them
+    // in some cases PARAMETERS MATRIX based I/O can be used => Parameterised tests
+
+    // in order to make possible unit testing of such private methods
+    // we can use the following approach
+    // 1. mark these private methods with default visibility
+    // 2. move these private methods to helper/companion/utility classes
+    // 3. mark encapsulating helper/companion/utility classes with default visibility
+    // 4. writing unit tests in classes defined in same package as encapsulating helper/companion/utility classes
+    // note that static methods approach do not help here
+
+    // AGAIN : IF IT IS HARD TO WRITE A UNIT TEST THEN THE PROBLEM IS NOT THE UNIT TEST, THE PROBLEM LIES IN THE DESIGN OF THE CODE
+
+}
---
+++ trainingUnitTests/src/test/java/dependencies/privates/ServiceWithPrivateDependenciesTest.java
@@ -0,0 +1,165 @@
+package dependencies.privates;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ServiceWithPrivateDependenciesTest {
+
+    @Test
+    public void testHappyPath() {
+
+        // 1
+        // create test context
+        ServiceWithPrivateDependencies service = new ServiceWithPrivateDependencies();
+
+        // 1
+        // 1.1
+        // create input data
+        int x = 11;
+        int y = 22;
+
+        // 1.2
+        // create expected result
+        int expectedResult = 33;
+
+        // 2
+        // 2.1 invoke to be tested method
+        // 2.2 and collect result
+        int actualResult = service.complexBusiness(x, y);
+
+        // 3
+        // verify expectations
+        // 3.1
+        // verify I/O expectations
+        Assert.assertEquals(expectedResult, actualResult);
+
+    }
+
+
+    @Test(expected = RuntimeException.class)
+    public void testSpecialCaseFirstParameterNegative() {
+
+        // 1
+        // create test context
+        ServiceWithPrivateDependencies service = new ServiceWithPrivateDependencies();
+
+        // 1
+        // 1.2
+        // create input data
+        int x = -1;
+        int y = 2;
+
+        // 1.1
+        // create expected result
+
+        // 2
+        // 2.1 invoke to be tested method
+        // 2.2 and collect result
+        try {
+            service.complexBusiness(x, y);
+        } catch (Exception e) {
+            Assert.assertEquals("Negative parameter", e.getMessage());
+            throw e;
+        }
+
+        // 3
+        // verify expectations
+        // 3.1
+        // verify I/O expectations
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testSpecialCaseSecondParameterNegative() {
+
+        // 1
+        // create test context
+        ServiceWithPrivateDependencies service = new ServiceWithPrivateDependencies();
+
+        // 1
+        // 1.1
+        // create input data
+        int x = 1;
+        int y = -2;
+
+        // 1.2
+        // create expected result
+
+        // 2
+        // 2.1 invoke to be tested method
+        // 2.2 and collect result
+        try {
+            service.complexBusiness(x, y);
+        } catch (Exception e) {
+            Assert.assertEquals("Negative parameter", e.getMessage());
+            throw e;
+        }
+
+        // 3
+        // verify expectations
+        // 3.1
+        // verify I/O expectations
+
+    }
+
+
+    @Test
+    public void testSpecialCaseFirstParameterLessThan10() {
+
+        // 1
+        // create test context
+        ServiceWithPrivateDependencies service = new ServiceWithPrivateDependencies();
+
+        // 1
+        // 1.1
+        // create input data
+        int x = 8;
+        int y = 10;
+
+        // 1.2
+        // create expected result
+        int expectedResult = 9;
+
+        // 2
+        // 2.1 invoke to be tested method
+        // 2.2 and collect result
+        int actualResult = service.complexBusiness(x, y);
+
+        // 3
+        // verify expectations
+        // 3.1
+        // verify I/O expectations
+        Assert.assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void testSpecialCaseSecondParameterLessThan10() {
+
+        // 1
+        // create test context
+        ServiceWithPrivateDependencies service = new ServiceWithPrivateDependencies();
+
+        // 1
+        // 1.1
+        // create input data
+        int x = 10;
+        int y = 8;
+
+        // 1.2
+        // create expected result
+        int expectedResult = 9;
+
+        // 2
+        // 2.1 invoke to be tested method
+        // 2.2 and collect result
+        int actualResult = service.complexBusiness(x, y);
+
+        // 3
+        // verify expectations
+        // 3.1
+        // verify I/O expectations
+        Assert.assertEquals(expectedResult, actualResult);
+
+    }
+}
